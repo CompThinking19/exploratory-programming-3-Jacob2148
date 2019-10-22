12c1595fb17f0847f1754ebc4fc5b7662fb55905
Jacob2148 <jtd145@scarletmail.rutgers.edu>
2019-10-22 12:21:47 -0400

    finished function search for words longer than 3 letters

--- Exp3.py
+++ Exp3.py
@@ -2,9 +2,12 @@ import re
 def search(book):
     if type(book) != str:
         raise TypeError("does not exist")
-    result = re.findall("[A-Za-z]+at\b", book)
+    result = re.findall("[A-Za-z]+at\\b", book)
     at_words = []
     for word in result:
         if len(word) > 3:
             at_words.append(word)
     return at_words
+source = open("expl3.txt")
+group = source.read()
+print search(group)
