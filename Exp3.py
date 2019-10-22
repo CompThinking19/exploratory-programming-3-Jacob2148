2f26d0e76891f5644c65e939d233fe370d065351
Jacob2148 <jtd145@scarletmail.rutgers.edu>
2019-10-22 12:02:01 -0400

    Regex function used to find words ending in at longer than 3 letters

--- Exp3.py
+++ Exp3.py
@@ -1,1 +1,10 @@
 import re
+def search(book):
+    if type(book) != str:
+        raise TypeError("does not exist")
+    result = re.findall("[A-Za-z]+at\b", book)
+    at_words = []
+    for word in result:
+        if len(word) > 3:
+            at_words.append(word)
+    return at_words
